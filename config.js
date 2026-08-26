400c15fba2a38e585378b9acde483959c42f578e
PalGenoPedia <palgeno.pedia@gmx.de>
2026-08-26 22:01:43 +0200

    Update to redeployed Web App URL (version 4)

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // the allow-list check inside Code.gs, not secrecy of these values).
 window.PORTAL_CONFIG = {
   // Apps Script Web App deployment URL ("Deploy > New deployment > Web app").
-  API_URL: "https://script.google.com/macros/s/AKfycbymFw611Tk625MvmFB9xvgdKwtSj6DyigTSW56q-3ECRXf0IEMUQpEhzuPQMBYjsMJ1ig/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbyRV3tYYe4qV9a2ULISPQ-5FcJRcNvbNTFuhJSH1tJb0QFxVHff3Dj-5B46Zz_JQiQFhA/exec",
 
   // OAuth 2.0 Client ID from Google Cloud Console (Web application type).
   GOOGLE_CLIENT_ID: "1017482285870-q0dl90l30asn736kad0u7qbucopj209a.apps.googleusercontent.com",
